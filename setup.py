7184e2ba546bc736cfa17338fee317576afba0b7
Gearlles <gearlles@gmail.com>
2015-03-24 00:21:38 -0300

    Import inicial.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='face-recognition-ar',
+    version='1.0.0',
+    packages=[],
+    url='',
+    license='',
+    author='Gearlles',
+    author_email='gvf@ecomp.poli.br',
+    description='Face recognition technique using AR database.',
+    install_requires=['PIL', 'matplotlib', 'numpy', 'facerec', "scipy"]
+)
